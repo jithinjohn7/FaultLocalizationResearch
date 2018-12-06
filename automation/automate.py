8a51374f79eb5c45ad952a53f945cf476f92d8c0
Jithin John <jjohnis@gmail.com>
2018-12-06 14:05:16 -0500

    adding tacoco integeration

--- automation/automate.py
+++ automation/automate.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 import collections
 import multiprocessing
+import json
 
 project_id = sys.argv[1]
 defect_id = sys.argv[2]
@@ -11,17 +12,24 @@ cwd=os.path.dirname(os.path.abspath(__file__))
 java6_home = "/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
 java8_home = "/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home"
 JAVA_HOME="JAVA_HOME"
+# tacoco_dir = "/Users/pranita/Downloads/Program_Repair/test-tacoco/tacoco"
 
-#os.system("export D4J_HOME=/Users/pranita/Downloads/Program_Repair/defects4j")
+# os.system("export D4J_HOME=/Users/pranita/Downloads/Program_Repair/defects4j")
 # os.environ['D4J_HOME'] = "/Users/pranita/Downloads/Program_Repair/defects4j"
 # os.environ['GZOLTAR_JAR'] = "/Users/pranita/Downloads/Program_Repair/fault-localization-data/gzoltar/gzoltar.jar"
 # os.environ['PATH'] += os.pathsep+"/Users/pranita/Downloads/Program_Repair/defects4j/framework/bin"
 # os.environ['SLOC_HOME']="/usr/local/bin/sloccount"
 
+
+# Downgrade maven version
+# os.system("brew install maven@3.2")
+# os.system("brew unlink maven")
+# os.system("brew link --force --overwrite maven@3.2")
+
 #ignored - bash get_fixed_lines.sh Lang 37 .
 
-if os.environ.get("D4J_HOME") is None or os.environ.get("GZOLTAR_JAR") is None or os.environ.get("SLOC_HOME"):
-    sys.exit('Error! Please set D4J_HOME, GZOLTAR_JAR and SLOC_HOME')
+#if os.environ.get("D4J_HOME") is None or os.environ.get("GZOLTAR_JAR") is None or os.environ.get("SLOC_HOME"):
+#    sys.exit('Error! Please set D4J_HOME, GZOLTAR_JAR and SLOC_HOME')
 
 d4j_dir=os.environ.get("D4J_HOME")
 
@@ -97,20 +105,20 @@ print("Running " + compile_invoke_command)
 os.system(compile_invoke_command)
 
 
-relevant_tests_list=None
-relevant_test_file=os.path.join(d4j_dir,"framework","projects",project_id,"relevant_tests",defect_id)
-with open(relevant_test_file) as f:
-    relevant_tests_list=f.read().splitlines()
+# relevant_tests_list=None
+# relevant_test_file=os.path.join(d4j_dir,"framework","projects",project_id,"relevant_tests",defect_id)
+# with open(relevant_test_file) as f:
+#     relevant_tests_list=f.read().splitlines()
 
-test_class_method_map = collections.OrderedDict()
+# test_class_method_map = collections.OrderedDict()
 
-for testclass in relevant_tests_list:
-    list_test_command="java -cp "+ ":".join(class_path) + " InvokeTests "+defect_dir + " getTestCases " + testclass
-    print("Running " + list_test_command)
-    tests_list = subprocess.run(list_test_command,stdout=subprocess.PIPE,shell=True)
-    tests_list = tests_list.stdout.decode("utf-8")
-    tests_list = tests_list.splitlines()
-    test_class_method_map[testclass] = tests_list
+# for testclass in relevant_tests_list:
+#     list_test_command="java -cp "+ ":".join(class_path) + " InvokeTests "+defect_dir + " getTestCases " + testclass
+#     print("Running " + list_test_command)
+#     tests_list = subprocess.run(list_test_command,stdout=subprocess.PIPE,shell=True)
+#     tests_list = tests_list.stdout.decode("utf-8")
+#     tests_list = tests_list.splitlines()
+#     test_class_method_map[testclass] = tests_list
     
 
 os.environ[JAVA_HOME]=java8_home
@@ -122,7 +130,20 @@ if not os.path.exists("./javaslicer"):
     
 tracer_dir = os.path.join(cwd,"javaslicer","assembly")
 
-#os.system("cp "+os.path.join(cwd,"javaslicer","assembly","*.jar")+" "+target_dir    )
+os.chdir(cwd)
+if not os.path.exists("./tacoco"):
+    os.system("git clone https://github.com/inf295uci-2015/primitive-hamcrest.git")
+    os.chdir("primitive-hamcrest")
+    os.system("mvn test")
+    os.system("mvn install")
+    os.chdir(cwd    )
+    os.system("git clone https://github.com/jithinjohn7/tacoco.git")
+    os.chdir("./tacoco")
+    os.system("mvn compile")
+
+tacoco_dir=os.path.join(cwd,"tacoco")
+
+# os.system("cp "+os.path.join(cwd,"javaslicer","assembly","*.jar")+" "+target_dir    )
 
 
 grep_tests_list=[]
@@ -135,71 +156,125 @@ relevant_class=""
 with open(os.path.join(gzoltars_dir,"spectra")) as f:
     relevant_class=f.read().splitlines()[0].split("#")[0]
 
+print(relevant_class)
+
+
+os.chdir(tacoco_dir)
+# tacoco_command_1="mvn -q exec:java -Plauncher -Dtacoco.sut="+defect_dir + " -Danalyzer.opts=configs/tacoco-analyzer.config"
+# print("Running command "+ tacoco_command_1)
+# os.system(tacoco_command_1)
+
+
+tacoco_command = "bash " + os.path.join(cwd,"tacoco","scripts","run-tacoco")+" " + defect_dir + " " + tacoco_dir
+print("Running command "+ tacoco_command)
+os.system(tacoco_command)
+
+#Read cov-matrix.json and find out activating tests
+
+cov_matrix_fileName = defect_id + "-cov-matrix.json"
+cov_matrix_filePath = os.path.join(tacoco_dir,"tacoco_output",cov_matrix_fileName)
+
+tacoco_result_dir=os.path.join(root_directory,"tacocos")
+
+if not os.path.exists(os.path.join(tacoco_result_dir,project_id)):
+    os.makedirs(os.path.join(tacoco_result_dir,project_id))
+os.rename(cov_matrix_filePath,os.path.join(tacoco_result_dir,project_id,cov_matrix_fileName))
+
+cov_matrix_filePath=os.path.join(tacoco_result_dir,project_id,cov_matrix_fileName)
+
+activating_tests = []
+with open(cov_matrix_filePath,'r') as f:
+    datastore = json.load(f)
+    masterTestsList = datastore['testsIndex']
+    masterTestsList = list(map(str,masterTestsList))
+    relevant = relevant_class.split('.')
+    javaFilePath = '/'.join(relevant) + '.java'
+    activating_tests_sequence = 0 
+    for i in range(len(datastore['sources'])):
+        if str(datastore['sources'][i]['source']['fullName']) == javaFilePath:
+            activating_tests_sequence = i
+            break
+
+    activating_tests_indexes = datastore['sources'][activating_tests_sequence]['activatingTests']
+    activating_tests = list(map(masterTestsList.__getitem__,activating_tests_indexes))
+def extractTest(s):
+    testName,testClass = s.split('(')
+    testClass = testClass.split(')')[0]
+    return [testClass,testName]
+activating_tests = list(map(extractTest,activating_tests))
+#print(len(activating_tests))
+print(activating_tests)
+
 os.chdir(os.path.join(cwd,"src"))
 if not os.path.exists(os.path.join(root_directory,"traces",project_id,defect_id)):
     os.makedirs(os.path.join(root_directory,"traces",project_id,defect_id))
 traces_dir = os.path.join(root_directory,"traces",project_id,defect_id)
 count=0
-for testclass in test_class_method_map:
-    for testname in test_class_method_map[testclass]:
-        if os.path.exists(os.path.join(traces_dir,"trace."+testclass+"#"+testname)):
-            print("Skipping! Tracer output already exists for "+testclass+"#"+testname)
-            continue
-        tracer_command="java -cp "+ ":".join(class_path) + " -javaagent:"+ \
-        os.path.join(tracer_dir,"tracer.jar")+"=tracefile:"+ \
-        os.path.join(traces_dir,"trace."+testclass+"#"+testname)+ \
-        " InvokeTests "+defect_dir+" runTestCase " + testclass + " " + testname
-        print("Running "+tracer_command)
-        os.system(tracer_command)
+
+#for testclass in test_class_method_map:
+ #   for testname in test_class_method_map[testclass]:
+
+for testclass,testname in activating_tests:
+    # if os.path.exists(os.path.join(traces_dir,"trace."+testclass+"#"+testname)):
+    #     print("Skipping! Tracer output already exists for "+testclass+"#"+testname)
+    #     continue
+    tracer_command="java -cp "+ ":".join(class_path) + " -javaagent:"+ \
+    os.path.join(tracer_dir,"tracer.jar")+"=tracefile:"+ \
+    os.path.join(traces_dir,"trace."+testclass+"#"+testname)+ \
+    " InvokeTests "+defect_dir+" runTestCase " + testclass + " " + testname
+    print("Running "+tracer_command)
+    os.system(tracer_command)
         
-        traceReader_command="java -jar "+os.path.join(tracer_dir,"traceReader.jar")+" "+ \
-        os.path.join(traces_dir,"trace."+testclass+"#"+testname) + " > " \
-        + os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output")
-        print("Running "+traceReader_command)
-        p = multiprocessing.Process(target=os.system(traceReader_command))
-        p.start()
+#         traceReader_command="java -jar "+os.path.join(tracer_dir,"traceReader.jar")+" "+ \
+#         os.path.join(traces_dir,"trace."+testclass+"#"+testname) + " > " \
+#         + os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output")
+#         print("Running "+traceReader_command)
+#         p = multiprocessing.Process(target=os.system(traceReader_command))
+#         p.start()
 
-        p.join(300)
+#         p.join(300)
 
-        if p.is_alive():
-            print("ERROR: running for too long... let's kill command: "+traceReader_command)
-            kill_count +=1
-            # Terminate
-            p.terminate()
-            p.join()
+#         if p.is_alive():
+#             print("ERROR: running for too long... let's kill command: "+traceReader_command)
+#             kill_count +=1
+#             # Terminate
+#             p.terminate()
+#             p.join()
 
-        grep_relevant_command="grep -rl "+relevant_class+" "+os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output")
-        print("Running "+grep_relevant_command)
-        grep_result = subprocess.run(grep_relevant_command,stdout=subprocess.PIPE,shell=True)
-        grep_result = grep_result.stdout.decode("utf-8")
-        if not grep_result is '':
-            grep_tests_list += grep_result.splitlines()
-        print("Count of grep result: "+str(len(grep_tests_list)))
-        print("Count killed test: "+str(kill_count))
+#         grep_relevant_command="grep -rl "+relevant_class+" "+os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output")
+#         print("Running "+grep_relevant_command)
+#         grep_result = subprocess.run(grep_relevant_command,stdout=subprocess.PIPE,shell=True)
+#         grep_result = grep_result.stdout.decode("utf-8")
+#         if not grep_result is '':
+#             grep_tests_list += grep_result.splitlines()
+#         print("Count of grep result: "+str(len(grep_tests_list)))
+#         print("Count killed test: "+str(kill_count))
 
-        print("Removing traceReader output file: "+os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output"))
-        os.remove(os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output"))
+#         print("Removing traceReader output file: "+os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output"))
+#         os.remove(os.path.join(traces_dir,"trace."+testclass+"#"+testname+".output"))
 
-    #     count+=1
-    #     if count==1:
-    #         break
-    # break
+#     #     count+=1
+#     #     if count==1:
+#     #         break
+#     # break
 
 
 
-print(grep_tests_list)
-for i in range(len(grep_tests_list)):
-    grep_tests_list[i] = os.path.basename(grep_tests_list[i])[6:]
-    grep_tests_list[i] = grep_tests_list[i].split("#")
-    grep_tests_list[i][1]=grep_tests_list[i][1][:-7]
+# print(grep_tests_list)
+# for i in range(len(grep_tests_list)):
+#     grep_tests_list[i] = os.path.basename(grep_tests_list[i])[6:]
+#     grep_tests_list[i] = grep_tests_list[i].split("#")
+#     grep_tests_list[i][1]=grep_tests_list[i][1][:-7]
 
-print("Count of grep result: "+str(len(grep_tests_list)))
+# print("Count of grep result: "+str(len(grep_tests_list)))
 
-with open(os.path.join(cwd,"test_count.txt"),"w") as f:
-    f.write("Count of grep result: "+str(len(grep_tests_list)))
+# with open(os.path.join(cwd,"test_count.txt"),"w") as f:
+#     f.write("Count of grep result: "+str(len(grep_tests_list)))
 
 os.chdir(os.path.join(cwd,"src"))
-for test in grep_tests_list:
+# print("GREP LIST")
+# print(grep_tests_list)
+for test in activating_tests:
     testclass=test[0]
     testname=test[1]
     assert_list_command="java -cp "+ ":".join(class_path) +  \
